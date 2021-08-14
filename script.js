5c1ebe12c81201ef5844bb3134f472cb2431b08c
Kendric Terry <41585980+Kendric285@users.noreply.github.com>
2021-08-14 15:02:02 +0000

    Upgraded Vaccine Finder

--- script.js
+++ script.js
@@ -2,10 +2,36 @@
 let submitButton = document.getElementById("zipCodeButton")
 let userZipCode = document.getElementById("userInput")
 let userState = document.getElementById("userState")
-let obj
+let zipCodes
+let apiKey = "cb74dbd635444e9498be72c410a9d156"
 
-submitButton.addEventListener("click", findVaccineCenters)
+// submitButton.addEventListener("click", findVaccineCenters)
+// submitButton.addEventListener("click" , covidInfo)
 
+submitButton.onclick = function(){
+  findVaccineCenters()
+  covidInfo()
+}
+
+function covidInfo(){
+  let state = userState.value
+  fetch('https://api.covidactnow.org/v2/state/' + state + '.json?apiKey=' + apiKey)
+    .then(function (response) {
+        console.log(response)
+        return response.json();
+    }).then(function (data) {
+        console.log(data.state)
+        //case density is new cases per 100k
+        let positiveTestRate = ((data.metrics.testPositivityRatio) * 100).toFixed(1) + "%"
+        let newDeaths = data.actuals.newDeaths
+        let newCases = data.actuals.newCases
+        
+        console.log(newDeaths + " new deaths")
+        console.log(newCases + " new cases")
+        console.log(positiveTestRate)
+
+    })
+}
 function findVaccineCenters(){
   let state = userState.value
   fetch('https://www.vaccinespotter.org/api/v0/states/'+ state +'.json')
@@ -15,51 +41,129 @@ function findVaccineCenters(){
       }).then(function (data) {
 
           let zip = userZipCode.value
-          let zipCodes = []
+          zipCodes = []
           let test = document.getElementById("test")
           let locations = data.features
-          //12211
-          // for(i in locations){
-          //   let location = data.features[i].properties['provider_brand_name'] + " at " + data.features[i].properties['address']
-          //   console.log(data.features[i].properties['provider_brand_name'] + " at " + data.features[i].properties['address'])
-          //   let l = document.createElement("p")
-          //   l.innerHTML = location
-          //   test.appendChild(l)
-          // }
           for(i in locations){
-            zipCodes.push(locations[i].properties["postal_code"])
-            let zip = data.features[i].properties['postal_code']
-            
+            zipCodes.push(
+              {
+                "zipCode": locations[i].properties["postal_code"],
+                "index": i
+              }
+              
+            )
+            // let zip = data.features[i].properties['postal_code']
           }
-          // for(i in zipCodes){
-          //   console.log(zipCodes[i])
-          // }
-          console.log(closest(zip,zipCodes))
-          for(i in obj){
+     
+          test.innerHTML = ""
+          // console.log(closest(zip))
+          // closest(zip)
+
+          console.log(findKClosestElements(zipCodes, 10, zip))
+
+          let closestCenters = findKClosestElements(zipCodes, 10, zip)
+          //
+
+          for(i in closestCenters){
             let l = document.createElement("p")
-            l.innerHTML = data.features[obj[i].index].properties['provider_brand_name'] + " at " + data.features[obj[i].index].properties['address']
+            l.innerHTML = data.features[closestCenters[i].index].properties['provider_brand_name'] + " at " + data.features[closestCenters[i].index].properties['address']
             test.appendChild(l)
           }
       })
 }
-
-
-function closest (num, arr){
-    let curr = arr[0]
+function closest (num){
+    let curr = zipCodes[0]
     let closeIndex
     obj = []
-    for(val in arr){
-      if (Math.abs(num - arr[val]) < Math.abs(num - curr)){
-        curr = arr[val]
-        closeIndex = val
-        obj.push(
-          {
-            "zipCode": curr,
-            "index": closeIndex
-          }
-        )
+    let inObj
+    let item = {}
+    for(i = 0; i < 10; i++){
+      for(val in zipCodes){ 
+        
+        if (Math.abs(num - zipCodes[val]) < Math.abs(num - curr)){
+            item = {
+              "zipCode": zipCodes[val],
+              "index": val
+            }
+            if(check(item) == true){
+              closeIndex = val
+              curr = zipCodes[val]
+
+              item = {
+                "zipCode": curr,
+                "index": closeIndex
+              }
+              addItem(item)
+            }
+        }
+        
       }
-    }
+      // console.log(closeIndex)
+      // zipCodes.splice(closeIndex, 1);
+      
+      
+    }   
     return obj
 }
+
+function addItem(item) {
+  var index = obj.findIndex(x => x.index == item.index)
+  if (index === -1) {
+    obj.push(item);
+  }else {
+    console.log("object already exists")
+  }
+}
+
+let testArray = [
+  {
+    "index": 5
+  },
+  {
+    "index": 10
+  },
+
+]
+
+
+function check(item) {
+  var index = obj.findIndex(x => x.index == item.index)
+  if (index === -1) {
+    return true
+  }else {
+    return false
+  }
+}
+
+function checkTest(item) {
+  var index = testArray.findIndex(x => x.index == item.index)
+  if (index === -1) {
+    return true
+  }else {
+    return false
+  }
+}
+
+let testItem = {
+  "index": 5
+}
+console.log(checkTest(testItem))
+
+function findKClosestElements(arr, k, x){
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (right - left >= k){
+        if(Math.abs(arr[left].zipCode - x) > Math.abs(arr[right].zipCode - x)){
+            left = left + 1;
+        }
+        else{
+            right = right - 1;
+        }
+
+    }
+    return arr.slice(left, left + k) 
+}
+
+
 
